ce146523fcb11669370ad7836fe31fec1bd63666
Exiam <121872598+Exiam6@users.noreply.github.com>
2024-04-24 20:30:25 +0800

    Update model.py

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ class RegressionResNet(nn.Module):
     def __init__(self, pretrained=True, num_outputs=2):
         super(RegressionResNet, self).__init__()
         self.model = models.resnet18(pretrained=pretrained)
-        self.model.conv1 = torch.nn.Conv2d(200, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        #self.model.conv1 = torch.nn.Conv2d(200, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
         num_ftrs = self.model.fc.in_features
         self.model.fc = nn.Linear(num_ftrs, num_outputs)
         self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
