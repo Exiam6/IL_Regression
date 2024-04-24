8f2bc7e87bfdd823ab545c594246ad43afee64f6
Exiam <121872598+Exiam6@users.noreply.github.com>
2024-04-24 20:31:15 +0800

    Update train.py

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ from tqdm import tqdm
 import os
 import random
 import json
+from plot import plot_metrics_over_epochs
 
 def to_serializable(val):
     """Converts NumPy arrays and PyTorch tensors to Python lists; converts NumPy and native Python numeric types to Python floats."""
@@ -125,12 +126,11 @@ def validate_epoch(model, epoch, val_data_loader, criterion, optimizer, device, 
         if args.debug:
             if count>10:
                 break
-        images = batch['image'].to(device)
+        images = batch['image'].to(device) #torch.Size([48, 3, 200, 88])
         targets = batch['target'].to(device)
         outputs = model(images)
         loss = criterion(outputs, targets)
         total_loss += loss.item() 
-        print(images.shape)
         if count < accum_size:
             embeddings = model.get_last_layer_embeddings(images)
             metrics['embeddings'] = torch.cat((metrics['embeddings'], embeddings.detach()), 0)
@@ -140,7 +140,7 @@ def validate_epoch(model, epoch, val_data_loader, criterion, optimizer, device, 
     metrics['loss'] = total_loss/count
     return metrics
 
-def train(model, train_data_loader, device, criterion, optimizer, args):
+def train(model, train_data_loader,val_data_loader, device, criterion, optimizer, args):
     all_results = {
         'cos_sim_y_Wh': [],
         'cos_sim_W': [],
@@ -168,8 +168,8 @@ def train(model, train_data_loader, device, criterion, optimizer, args):
         'W_norm':[],
     }
     for epoch in range(1, args.num_epochs + 1):
-        metrics = train_epoch(model, epoch,train_data_loader, criterion, optimizer, device, args, accum_size=200 )
-        metrics_valid = validate_epoch(model, epoch,train_data_loader, criterion, optimizer, device, args, accum_size=100 )
+        metrics = train_epoch(model, epoch,train_data_loader, criterion, optimizer, device, args, accum_size=10 )
+        metrics_valid = validate_epoch(model, epoch,val_data_loader, criterion, optimizer, device, args, accum_size=10 )
        
         result = calculate_metrics(metrics, device, args.y_dim)
         result_valid = calculate_metrics(metrics_valid, device, args.y_dim)
@@ -177,7 +177,7 @@ def train(model, train_data_loader, device, criterion, optimizer, args):
             all_results[key].append(result[key])
         for key in all_results_valid:
             all_results_valid[key].append(result_valid[key])
-        print(all_results,all_results_valid)
+
         plot_metrics_over_epochs(all_results, all_results_valid, epoch, args.save_dir)
 
         if epoch % 10 ==0:
@@ -192,7 +192,10 @@ def calculate_metrics(metrics, device, y_dim):
     Wh = metrics['outputs'].to(device) #(B,2)
     W = metrics['weights'].to(device) #(2,512)
     H = metrics['embeddings'].to(device) #(B,512)
-
+    print("Y",y.shape)
+    print("Wh",Wh.shape)
+    print("W",W.shape)
+    print("H",H.shape)
     H_norm = F.normalize(H, p=2, dim=1)
     y_norm = F.normalize(y, p=2, dim=1)
     W_norm = F.normalize(W, p=2, dim=1)
@@ -200,7 +203,9 @@ def calculate_metrics(metrics, device, y_dim):
     result['loss'] = metrics['loss']
     # Cosine similarity calculations
     result['cos_sim_y_Wh'] = cosine_similarity_gpu(y,Wh).mean().item()
+    print(cosine_similarity_gpu(W, W))
     result['cos_sim_W'] = cosine_similarity_gpu(W, W).fill_diagonal_(float('nan')).nanmean().item()
+    print(result['cos_sim_W'])
     result['cos_sim_H'] = cosine_similarity_gpu(H_norm, H_norm).fill_diagonal_(float('nan')).nanmean().item()
 
     # H with PCA
@@ -232,8 +237,9 @@ def calculate_metrics(metrics, device, y_dim):
     # Projection error with Gram-Schmidt
     U = gram_schmidt(W)
     P_E = torch.mm(U.T, U)
-    H_projected_E = torch.mm(H, P_E)
-    result['projection_error_H2W_E'] = F.mse_loss(H_projected_E, H).item()
+    H_projected_E = torch.mm(H_norm, P_E)
+    H_projected_E_norm = F.normalize(torch.tensor(H_projected_E).float().to(device), p=2, dim=1)
+    result['projection_error_H2W_E'] = F.mse_loss(H_projected_E_norm, H_norm).item()
 
     # Cosine similarity of Y and H with H2W
     H_coordinates = torch.mm(H, U.T)
@@ -243,93 +249,3 @@ def calculate_metrics(metrics, device, y_dim):
 
     print(result)
     return result
-
-def plot_metrics_over_epochs(all_results, all_results_valid, epoch, save_dir):
-    
-    # Plotting loss
-    plt.figure(figsize=(10, 6))
-    plt.plot(range(1, epoch + 1), all_results['loss'], label="Train", color='blue')
-    plt.plot(range(1, epoch + 1), all_results_valid['loss'], label="Test", color='red')
-    plt.title('Train and Test W_norm Over Epochs')
-    plt.xlabel('Epoch')
-    plt.ylabel('W_norm')
-    plt.legend()
-    plt.grid(True)
-    plt.savefig(f"{save_dir}/W_norm.png")
-    plt.close()
-
-    # Plotting W_norm
-    plt.figure(figsize=(10, 6))
-    plt.plot(range(1, epoch + 1), all_results['W_norm'], label="Train",color='blue')
-    plt.plot(range(1, epoch + 1), all_results_valid['W_norm'], label="Train",color='red')
-    plt.title('Train and Test Loss Over Epochs')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.legend()
-    plt.grid(True)
-    plt.savefig(f"{save_dir}/Loss.png")
-    plt.close()
-
-    # Plotting cosine similarities
-    plt.figure(figsize=(10, 6))
-    metrics_cosine = ['cos_sim_y_Wh', 'cos_sim_W', 'cos_sim_H', 'cos_sim_y_h_postPCA','cos_sim_y_h_H2W_E']
-    colors = ['blue', 'green', 'red', 'purple', 'orange'] 
-    for metric, color in zip(metrics_cosine, colors):
-        plt.plot(range(1, epoch + 1), all_results[metric], label=metric, color=color)
-    plt.title('Train Cosine Similarities Over Epochs')
-    plt.xlabel('Epoch')
-    plt.ylabel('Cosine Similarity')
-    plt.legend()
-    plt.grid(True)
-    plt.savefig(f"{save_dir}/cosine_similarities_train.png")
-    plt.close()
-
-    # Plotting cosine similarities
-    plt.figure(figsize=(10, 6))
-    metrics_cosine = ['cos_sim_y_Wh', 'cos_sim_W', 'cos_sim_H', 'cos_sim_y_h_postPCA','cos_sim_y_h_H2W_E']
-    for metric, color in zip(metrics_cosine, colors):
-        plt.plot(range(1, epoch + 1), all_results_valid[metric], label=metric, color=color)
-    plt.title('Test Cosine Similarities Over Epochs')
-    plt.xlabel('Epoch')
-    plt.ylabel('Cosine Similarity')
-    plt.legend()
-    plt.grid(True)
-    plt.savefig(f"{save_dir}/cosine_similarities_test.png")
-    plt.close()
-
-    # Plotting projection errors in one plot
-    plt.figure(figsize=(10, 6))
-    metrics_projection = ['projection_error_PCA', 'projection_error_H2W_E']
-    train_colors = ['cyan', 'magenta']  
-    test_colors = ['darkcyan', 'darkmagenta']  
-    for metric, color in zip(metrics_projection, train_colors):
-        plt.plot(range(1, epoch + 1), all_results[metric], label=f"Train {metric}", color=color)
-    for metric, color in zip(metrics_projection, test_colors):
-        plt.plot(range(1, epoch + 1), all_results_valid[metric], label=f"Test {metric}", color=color)
-    plt.title('Projection Errors Over Epochs')
-    plt.xlabel('Epoch')
-    plt.ylabel('Projection Error')
-    plt.legend()
-    plt.grid(True)
-    plt.savefig(f"{save_dir}/projection_errors_up_to_epoch.png")
-    plt.close()
-
-    # Plotting MSE cosine similarity
-    plt.figure(figsize=(10, 6))
-    plt.plot(range(1, epoch + 1), all_results['mse_cos_sim'], label='Train ', color='magenta')
-    plt.plot(range(1, epoch + 1), all_results_valid['mse_cos_sim'], label='Test', color='darkmagenta')
-    plt.plot(range(1, epoch + 1), all_results['mse_cos_sim_PCA'], label='Train ', color='cyan')
-    plt.plot(range(1, epoch + 1), all_results_valid['mse_cos_sim_PCA'], label='Test',color='darkcyan')
-    
-    plt.title('MSE of Cosine Similarities Over Epochs')
-    plt.xlabel('Epoch')
-    plt.ylabel('MSE Cosine Similarity')
-    plt.legend()
-    plt.grid(True)
-    plt.savefig(f"{save_dir}/mse_cosine_similarity_up_to_epoch.png")
-    plt.close()
-
-    print(f"Metrics plotted and saved up to epoch")
-
-
-
