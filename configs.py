dc36a7ca6b62897c1bd507086e01b19820007e9a
Exiam <121872598+Exiam6@users.noreply.github.com>
2024-08-16 13:55:10 +0800

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,17 +3,21 @@ import torch
 
 def get_args():
     parser = argparse.ArgumentParser(description='Training Configuration')
-    parser.add_argument('--save_dir', type=str, default="/scratch/zz4330/IL_Regression_2D_V2/Result/2DCase2_W5e-2H1e-5", help='Directory to save results')
+    parser.add_argument('--random_seed', type=int, default=43, help='Random seed')
+    parser.add_argument('--save_dir', type=str, default="/scratch/zz4330/IL_Regression_2D_V2/Result_New/Case1_W5e-3/", help='Directory to save results')
+    parser.add_argument('--lambda_W', type=float, default=5e-1, help='Regularization lambda W')
+    parser.add_argument('--pca_n', type=int, default=2, help='Dimension of pca components')
+    parser.add_argument('--case2', type=int, default=0, help='Enable case 2 scenario')
+    parser.add_argument('--bias', default=True, action='store_true', help='Enable bias scenario')
     parser.add_argument('--checkpoint_path', type=str, default="/scratch/zz4330/IL_Regression/Result/Case2_W5e-2H5e-5/checkpoints/model_checkpoint_epoch_160.pth", help='Path to model checkpoint')
-    parser.add_argument('--case2', default=True, action='store_true', help='Enable case 2 scenario')
     parser.add_argument('--y_dim', type=int, default=2, help='Dimension of output')
     parser.add_argument('--debug', default=False, help='Only runs 20 batches per epoch for debugging, and set debug to true', action='store_true')
     
     parser.add_argument('--num_epochs', type=int, default=500, help='Number of epochs')
     parser.add_argument('--batch_size', type=int, default=512, help='Batch size')
     parser.add_argument('--learning_rate', type=float, default=1e-3, help='Learning rate')
-    parser.add_argument('--lambda_H', type=float, default=1e-5, help='Regularization lambda H')
-    parser.add_argument('--lambda_W', type=float, default=5e-2, help='Regularization lambda W')
+    parser.add_argument('--lambda_H', type=float, default=1e-1, help='Regularization lambda H')
+    
     
     parser.add_argument('--train_data_path', type=str, default='/scratch/zz4330/Carla/Train/images.npy', help='Training data path')
     parser.add_argument('--train_target_path', type=str, default='/scratch/zz4330/Carla/Train/targets.npy', help='Training target path')
@@ -24,7 +28,7 @@ def get_args():
     
     parser.add_argument('--sampling_rate', type=float, default=0.1, help='Sampling rate for training')
     parser.add_argument('--start_epoch', type=int, default=81, help='Starting epoch number')
-    parser.add_argument('--random_seed', type=int, default=43, help='Random seed')
+    
     return parser.parse_args()
 
 def main():
@@ -40,4 +44,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
