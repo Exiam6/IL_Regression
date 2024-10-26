bcecac26f02825bf7f2afe8405eeddb3c4bbdfb5
Zifan Zhao <121872598+Exiam6@users.noreply.github.com>
2024-10-26 10:43:04 -0400

    Update model.py

--- model.py
+++ model.py
@@ -6,12 +6,16 @@ class RegressionResNet(nn.Module):
     def __init__(self, pretrained=True, bias=False, num_outputs=2):
         super(RegressionResNet, self).__init__()
         resnet_model = models.resnet18(pretrained=pretrained)
-        self.backbone = nn.Sequential(*list(resnet_model.children())[:-2])
+        
+        # Modify backbone to exclude the last layer containing ReLU
+        self.backbone = nn.Sequential(*list(resnet_model.children())[:-1])  # Excludes the final layer (which has ReLU)
         self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        
         self.feat = nn.Sequential(
             nn.Flatten(), 
             nn.Linear(resnet_model.fc.in_features, resnet_model.fc.in_features)
         )
+        
         self.fc = nn.Linear(resnet_model.fc.in_features, num_outputs, bias=bias)
         self.embeddings = None
 
@@ -24,11 +28,10 @@ class RegressionResNet(nn.Module):
     def get_last_layer_embeddings(self, x):
         """Extract embeddings from the last layer using a hook and global average pooling."""
         def hook_fn(module, input, output):
-            pooled_output = self.global_avg_pool(output)
-            self.embeddings = pooled_output.view(pooled_output.size(0), -1).detach()
-
+            self.embeddings = output.detach() 
+            
         # Register the hook on the appropriate layer
-        hook = self.backbone[-1].register_forward_hook(hook_fn)
+        hook = self.feat[-1].register_forward_hook(hook_fn)
         self.forward(x)
         hook.remove()
         return self.embeddings
